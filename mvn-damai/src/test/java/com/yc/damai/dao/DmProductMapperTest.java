1bd3a4a02de8930454a948465056002cb4d6376e
1079368619 <1079368619@qq.com>
2020-08-02 16:45:46 +0800

    log4j的配置

---
+++ mvn-damai/src/test/java/com/yc/damai/dao/DmProductMapperTest.java
@@ -0,0 +1,38 @@
+package com.yc.damai.dao;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import com.yc.damai.po.DmProduct;
+
+public class DmProductMapperTest {
+
+	@Test
+	public void test1() throws IOException {
+		//
+		String resource = "mybatis.xml";
+		//
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		//
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		//
+		SqlSession session = sqlSessionFactory.openSession();
+		
+		//
+		List<DmProduct> list = session.selectList("com.yc.damai.dao.ProductMapper.selectAll");
+		
+		/**
+		for(DmProduct dp : list) {
+			System.out.println(dp);
+		}
+		*/
+	}
+
+}
