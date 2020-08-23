45cd46dbadbbb3ec652ae6342041e553849188ff
1079368619 <1079368619@qq.com>
2020-08-23 14:59:47 +0800

    项目基础版本

---
+++ C83-S3-Ply-blog/src/main/java/com/yc/C83S3Plyblog/C83S3PlyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yc.C83S3Plyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C83S3PlyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C83S3PlyBlogApplication.class, args);
+	}
+
+}
