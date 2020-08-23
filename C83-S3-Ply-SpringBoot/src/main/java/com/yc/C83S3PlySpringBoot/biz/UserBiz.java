2bff110264f197c885e212f2d0d13ae167b91074
1079368619 <1079368619@qq.com>
2020-08-23 14:33:58 +0800

    0822

---
+++ C83-S3-Ply-SpringBoot/src/main/java/com/yc/C83S3PlySpringBoot/biz/UserBiz.java
@@ -0,0 +1,29 @@
+package com.yc.C83S3PlySpringBoot.biz;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.yc.C83S3PlySpringBoot.dao.UserDao;
+import com.yc.damai.bean.DmUser;
+
+/**
+ * 
+ * 李四开发 UserBiz 已经完成
+ *
+ */
+@Service
+public class UserBiz {
+
+	@Resource
+	private UserDao udao;
+	
+	public DmUser login(String name, String pwd) throws BizException {
+		DmUser user = udao.selectByLogin(name, pwd);
+		
+		if(user == null) {
+			throw new BizException("用户名或密码错误！");
+		}
+		return user ;
+	}
+}
--- C83-S3-Ply-SpringBoot/src/test/java/com/yc/C83S3PlySpringBoot/C83S3PlySpringBootApplicationTests.java
+++ C83-S3-Ply-SpringBoot/src/test/java/com/yc/C83S3PlySpringBoot/C83S3PlySpringBootApplicationTests.java
@@ -1,13 +1,30 @@
 package com.yc.C83S3PlySpringBoot;
 
+import javax.annotation.Resource;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import com.yc.C83S3PlySpringBoot.biz.MailService;
+import com.yc.C83S3PlySpringBoot.dao.ProductMapper;
 
 @SpringBootTest
 class C83S3PlySpringBootApplicationTests {
 
+	@Resource
+	ProductMapper pm;
+	
+	@Resource
+	MailService ms;
+	
 	@Test
 	void contextLoads() {
+		Assert.isTrue(pm.selectAll().size() > 0, "没有数据");
 	}
 
+	@Test
+	void testMail() {
+		ms.sendSimpleMail("1079368619@qq.com", "密码重置验证码", "您重置密码的验证码是: 123456");
+	}
 }
