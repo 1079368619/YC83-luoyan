07e6820727f9295380fc3790404492d45770a8ef
1079368619 <1079368619@qq.com>
2020-08-27 19:14:25 +0800

    2

--- C83-S3-Ply-cloud-order/src/main/java/com/yc/order/OrderApplication.java
+++ C83-S3-Ply-cloud-order/src/main/java/com/yc/order/OrderApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class OrderApplication {
+public class OrderApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
