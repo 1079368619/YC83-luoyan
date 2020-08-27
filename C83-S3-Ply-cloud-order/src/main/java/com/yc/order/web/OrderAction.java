23cba0e54ea929c8a2dce0ae8d74e3b524a90829
1079368619 <1079368619@qq.com>
2020-08-27 10:42:11 +0800

    1

---
+++ C83-S3-Ply-cloud-order/src/main/java/com/yc/order/web/OrderAction.java
@@ -0,0 +1,18 @@
+package com.yc.order.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderAction {
+
+	@GetMapping("order")
+	public String order() {
+		return "order";
+	}
+	
+	@GetMapping("user")
+	public String user() {
+		return "user";
+	}
+}
---
+++ C83-S3-Ply-cloud-user/src/main/java/com/yc/user/UserApplication.java
@@ -0,0 +1,23 @@
+package com.yc.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class UserApplication implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+	
+	/**
+	 * 	定义 RestTemplate  Bean
+	 */
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
